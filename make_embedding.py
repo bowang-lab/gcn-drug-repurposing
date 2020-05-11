eae0b83369bb9cec0d436461f4bda760168fc7c2
subercui <subercui@gmail.com>
2020-05-10 22:57:41 -0400

    [update] add make_embedding

---
+++ make_embedding.py
@@ -0,0 +1,17 @@
+# test proximity
+import multiprocessing
+from joblib import Parallel, delayed
+import numpy as np
+import pandas as pd
+
+from toolbox import wrappers, network_utilities
+from utils import load_drugs_from, load_diseases_from
+import time
+
+# load data
+
+# graph embedding
+
+# train the gcn
+
+# make predictions
