ba6aa3b51479a98006fc68944c7ff590881d1972
subercui <subercui@gmail.com>
2020-05-29 13:36:28 -0400

    [update] evaluate auc working

--- evaluate_auc.py
+++ evaluate_auc.py
@@ -5,6 +5,7 @@
 from sklearn.preprocessing import normalize
 from multiscale_interactome.openne.node2vec import Node2vec
 from multiscale_interactome.openne.graph import Graph
+from multiscale_interactome.msi.node_to_node import DrugToIndication
 import os
 from multiscale_interactome.msi.msi import MSI
 from multiscale_interactome.diff_prof.diffusion_profiles import DiffusionProfiles
@@ -12,6 +13,7 @@ import multiprocessing
 import numpy as np
 import pickle
 import networkx as nx
+from sklearn.metrics import roc_auc_score
 
 import pandas as pd
 import urllib
@@ -21,49 +23,100 @@ from parse_config import ConfigParser
 from utils import query_uniprot2data, make_SARSCOV2_PPI
 
 
+def graph_embedding(config, msi, gcn=False):
+    # if node2vec
+    # debug_g = nx.subgraph(msi.graph, list(g.nodes.keys())[:40])
+    emb_file_prefix = config['node2vec']['eval_emb_file_prefix']
+    walk_length = config['node2vec']['walk_length']
+    number_walks = config['node2vec']['number_walk']
+    emb_file = f"{emb_file_prefix}_num_{number_walks}_len_{walk_length}.embs.txt"
+    gcn_emb_file = config['gcn']['emb_file']
+    if not os.path.exists(emb_file):
+        print("Calculating node2vec embeddings...")
+        g = Graph()
+        g.read_g(msi.graph)
+        model = Node2vec(
+            graph=g, path_length=walk_length,
+            num_paths=number_walks, dim=128,
+            workers=8, p=0.25, q=0.25, window=10
+        )
+        print("Saving embeddings...")
+        model.save_embeddings(emb_file)
+
+    # load embs
+    node_vecs = np.loadtxt(emb_file, skiprows=1, dtype=object)
+    node_names = list(node_vecs[:, 0])
+    if gcn:
+        node_embs = np.loadtxt(gcn_emb_file)
+        node_embs = normalize(node_embs, axis=1)
+    else:
+        node_embs = node_vecs[:, 1:].astype(np.float)
+
+    # FIXME: modify after this line
+    covid_emb = np.array(node_embs[node_names.index('NodeCovid')])
+
+    drug_names = []
+    drug_embs = []
+    for i, node in enumerate(node_names):
+        if msi.graph.nodes[node]['type'] == 'drug':
+            drug_names.append(node)
+            drug_embs.append(node_embs[i])
+    drug_embs = np.array(drug_embs)
+    proximities = np.matmul(drug_embs, covid_emb)
+    # drug_embs_normed = normalize(drug_embs, axis=1)
+    # proximities = np.matmul(drug_embs_normed, covid_emb)
+
+    proximities_ranked_id = np.argsort(np.array(proximities))[::-1]
+    drugs_ranked = [drug_names[i] for i in proximities_ranked_id]
+    drugs_name_ranked = [drug if msi.node2name[drug]
+                         is np.nan else msi.node2name[drug] for drug in drugs_ranked]
+    return drugs_name_ranked
+
+
+def diffusion_method(diffusion_embs_dir, msi):
+    if not os.path.exists(diffusion_embs_dir):
+        # Calculate diffusion profiles
+        print('Calculate diffusion profiles')
+        dp = DiffusionProfiles(
+            alpha=0.8595436247434408,
+            max_iter=1000,
+            tol=1e-06,
+            weights={
+                'down_functional_pathway': 4.4863053901688685,
+                'indication': 3.541889556309463,
+                'functional_pathway': 6.583155399238509,
+                'up_functional_pathway': 2.09685000906964,
+                'protein': 4.396695660380823,
+                'drug': 3.2071696595616364
+            },
+            num_cores=int(multiprocessing.cpu_count()/2),
+            save_load_file_path=diffusion_embs_dir
+        )
+        dp.calculate_diffusion_profiles(msi)
+    # Load saved diffusion profiles
+    dp_saved = DiffusionProfiles(
+        alpha=None,
+        max_iter=None,
+        tol=None,
+        weights=None,
+        num_cores=None,
+        save_load_file_path=diffusion_embs_dir
+    )
+    msi.load_saved_node_idx_mapping_and_nodelist(dp_saved.save_load_file_path)
+    dp_saved.load_diffusion_profiles(
+        msi.drugs_in_graph + msi.indications_in_graph)
+
+    return dp_saved
+
+
 def main(config):
     method = config['method']
-    save_drug_candidates_dir = config['output']['drug_candidates']
-    save_graph_file = config['output']['graph']
-    topk = config['topk']
-    gordon_viral_protein = config['networks']['gordon_viral_protein']
-    covid_to_protein = config['covid']['save_dir']
-    protein_to_protein = config['networks']['protein_to_protein']
-    diffusion_embs_dir = config['diffusion']['diffusion_embs_dir']
-
-    if not os.path.exists(covid_to_protein):
-        # load proteins
-        proteins = pd.read_csv(protein_to_protein, sep='\t')
-
-        covid_protein_list = make_SARSCOV2_PPI(gordon_viral_protein)  # 332
-        # has 306
-        covid_protein_list = [
-            protein for protein in covid_protein_list if protein in proteins['node_1_name'].values]
-
-        proteinname2node = dict(
-            set(list(zip(proteins['node_1_name'], proteins['node_1']))))
-        # make covid data fram
-        node_1 = ['NodeCovid'] * len(covid_protein_list)
-        node_1_type = ['indication'] * len(covid_protein_list)
-        node_1_name = ['covid-19'] * len(covid_protein_list)
-        node_2 = [proteinname2node[name] for name in covid_protein_list]
-        node_2_type = ['protein'] * len(covid_protein_list)
-        node_2_name = covid_protein_list
-        covid_protein_df = pd.DataFrame({
-            'node_1': node_1,
-            'node_2': node_2,
-            'node_1_type': node_1_type,
-            'node_2_type': node_2_type,
-            'node_1_name': node_1_name,
-            'node_2_name': node_2_name
-        })
-        covid_protein_df.to_csv(covid_to_protein, sep='\t', index=False)
-    else:
-        covid_protein_df = pd.read_csv(covid_to_protein, sep='\t')
+    save_graph_file = config['eval']['graph']
+    diffusion_embs_dir = config['diffusion']['eval_diffusion_embs_dir']
+    drug_to_indication = config['networks']['drug_to_indication']
 
     # Construct the multiscale interactome
-    msi = MSI(indication2protein_file_path=covid_to_protein,
-              indication2protein_directed=False)
+    msi = MSI()
     msi.load()
 
     # store the whole graph
@@ -76,15 +129,45 @@ def main(config):
 
     # nx.write_edgelist(msi.graph, 'whole_graph.edgelist')
     if method == 'diffusion':
-        drugs_name_ranked = diffusion_method(diffusion_embs_dir, msi)
+        dp_saved = diffusion_method(diffusion_embs_dir, msi)
     elif method == 'node2vec':
         drugs_name_ranked = graph_embedding(config, msi, gcn=False)
     elif method == 'gcn' and config['gcn']['embs'] == "node2vec":
         drugs_name_ranked = graph_embedding(config, msi, gcn=True)
     else:
         raise NotImplementedError
-    with open(save_drug_candidates_dir, 'w') as f:
-        f.write('\n'.join(drugs_name_ranked[:topk]))
+
+    indication_graph = DrugToIndication(False, drug_to_indication)
+    # list(indication_graph.graph['C0040038'])
+
+    drugs_index_in_msi = []
+    drugs = []
+    indications_index_in_msi = []
+    indications = []
+    for i, node in enumerate(msi.nodelist):
+        if msi.graph.nodes[node]['type'] == 'drug':
+            drugs.append(node)
+            drugs_index_in_msi.append(i)
+        if msi.graph.nodes[node]['type'] == 'indication':
+            indications.append(node)
+            indications_index_in_msi.append(i)
+    # list of indications
+
+    num_drugs = len(drugs_index_in_msi)
+    all_aucs = []
+    for indication in indications:
+        # build ref
+        ref = np.zeros(num_drugs, dtype=int)
+        for pos_drug in list(indication_graph.graph[indication]):
+            ref[drugs.index(pos_drug)] = 1
+        # predict vector
+        tmp = dp_saved.drug_or_indication2diffusion_profile[indication]
+        predict = tmp[drugs_index_in_msi]
+        auc = roc_auc_score(ref, predict)
+        # print(auc)
+        all_aucs.append(auc)
+    all_aucs = np.array(all_aucs)
+    print(f"median auc: {np.median(all_aucs)}, mean auc: {all_aucs.mean()}")
     return msi
 
 
